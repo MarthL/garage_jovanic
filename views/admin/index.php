857742cf9af5dd9a3b967305eb85900957052cf6
martin <martin.aubertin@90tech.fr>
2022-09-04 19:32:37 +0200

    ajout du projet

---
+++ views/admin/index.php
@@ -0,0 +1,45 @@
+
+<div class="ban" id="ban__admin">
+<h1>Administration</h1>
+</div>
+
+<div class="admin__container">
+ <div class="admin-flex">
+    <div class="admin__content">
+        <img src="image/admin-utilisateur.png" alt="">
+        <h3>Mes utilisateurs</h3>
+        <a href="admin/utilisateurs/index">Accéder</a>
+    </div>
+
+    <div class="admin__content">
+        <img src="image/admin-calender.png" alt="">
+        <h3>Mes rendez-vous</h3>
+        <a href="">Accéder</a>
+    </div>
+    <div class="admin__content">
+        <img src="image/admin-calender.png" alt="">
+        <h3>Mes prestations</h3>
+        <a href="admin/prestations/index">Accéder</a>
+    </div>
+</div>
+<div class="admin-flex">
+    <div class="admin__content">
+        <img src="image/admin-message.png" alt="">
+        <h3>Mes messages</h3>
+        <a href="admin/message/index">Accéder</a>
+    </div>
+
+    <div class="admin__content">
+        <img src="image/admin-annonce.png" alt="">
+        <h3>Mes annonces</h3>
+        <a href="admin/annonces/index">Accéder</a>
+    </div>
+</div>
+</div>
+
+
+
+<div class="admin__btn">
+<a href="Compte/logout">Deconnexion</a>
+</div>
+
